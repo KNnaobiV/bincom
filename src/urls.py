06406cfbc555c7d24fcf60dbb0ee8a3855a662e5
Nnaobi Godson <46877807+KNnaobiV@users.noreply.github.com>
2025-01-20 02:54:27 +0100

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from bincomTest.urls import urlpatterns
 
-urlpatterns = [
+urlpatterns += [
     path('admin/', admin.site.urls),
-    path('bincomtest/', include("bincomTest.urls"))
 ]
